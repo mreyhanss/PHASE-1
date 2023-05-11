8bf0fbbbe6fd05498954bf28521ad1f185e09cdd
mreyhanss <me.reyhann@gmail.com>
2023-05-11 21:17:43 +0700

    Refresh Week2

--- Week2/Array.js
+++ Week2/Array.js
@@ -6,24 +6,43 @@
 // }
 // console.log(antrian(anggota, 'Agung'));
 
-function ganjilGenap(platNomor) {
+// function ganjilGenap(platNomor) {
 
-    let platGanjil = 0
-    let platGenap = 0
+//     let platGanjil = 0
+//     let platGenap = 0
 
-    for (let i = 0; i < platNomor.length; i++) {
-        if (platNomor[i] % 2 === 0) {
-            platGenap += 1;
-        } else {
-            platGanjil += 1;
-        }
-    }
-    console.log('plat genap sebanyak', platGenap, 'dan plat ganjil sebanyak', platGanjil);
-}
+//     for (let i = 0; i < platNomor.length; i++) {
+//         if (platNomor[i] % 2 === 0) {
+//             platGenap += 1;
+//         } else {
+//             platGanjil += 1;
+//         }
+//     }
+//     console.log('plat genap sebanyak', platGenap, 'dan plat ganjil sebanyak', platGanjil);
+// }
 
-ganjilGenap([2341, 3429, 864, 1309, 1276])//plat genap sebanyak 2 dan plat ganjil sebanyak 3
+// ganjilGenap([2341, 3429, 864, 1309, 1276])//plat genap sebanyak 2 dan plat ganjil sebanyak 3
 // console.log(ganjilGenap('2347;3429;1305')) //plat ganjil sebanyak 3 dan plat genap tidak ditemukan
 // console.log(ganjilGenap('864;1308;1276;1432')) //plat genap sebanyak 4 dan plat ganjil tidak ditemukan
 // console.log(ganjilGenap('')) //plat tidak ditemukan
 // console.log(ganjilGenap()) //invalid data
 
+
+let board = ['i', '', '', 10, '', '', -5, -10, '', 100, 'a', 'A', 'o', 'b']
+let vocalan = ['a', 'i', 'u', 'e', 'o']
+let jumlahan = 0
+let tampunghuruf = ''
+
+function vocalSeeker(draob) {
+    for (let i = 0; i < draob.length; i++) {
+        for (let j = 0; j < vocalan.length; j++) {
+            if (String(draob[i]).toLowerCase() === vocalan[j]) {
+                jumlahan += 1
+                tampunghuruf += draob[i]
+            }
+        }
+    }
+    return `vokal ditemukan ${jumlahan} dan kumpulan vokal adalah ${tampunghuruf}`
+}
+
+console.log(vocalSeeker(board)); // vokal ditemukan 3 dan kumpulan vokal adalah aAo
---
+++ Week2/CekLogUlang.js
@@ -0,0 +1,45 @@
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i, 'ini lari lap ke', i);
+// }
+
+// function gaoel(number1) {
+
+//     let tampungangka = ''
+
+//     for (let i = 1; i <= number1; i++) {
+//         for (let j = 1; j <= number1; j++) {
+//             tampungangka += '*'
+//         }
+//         console.log(tampungangka);
+//         tampungangka = ''
+//     }
+// }
+
+// gaoel(3)
+
+// function kopinama(nama1) {
+//     let namakosong = ''
+//     for (let i = nama1.length - 1; i >= 0; i--) {
+//         namakosong += nama1[i]
+
+//     }
+//     console.log(namakosong);
+// }
+
+// kopinama('LumbaLumba')
+
+// function namaa(gasgas) {
+//     let namakosong1 = ''
+//     for (let i = gasgas.length - 1; i >= 0; i--) {
+//         namakosong1 += gasgas[i]
+
+//     } console.log(namakosong1);
+
+//     if (namakosong1 === gasgas) {
+//         console.log('Ini Palindrom');
+//     } else {
+//         console.log('Ini bukan Palindrom');
+//     }
+// }
+
+// namaa('KASURRUSAK')
